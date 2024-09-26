2840d5f8dcd526c605e29bca1e11693c2e42f0f4
abuu94 <abuu.yus@gmail.com>
2024-09-26 15:03:02 +0300

    Updated

---
+++ first-app_01-hello-world/src/app/housinglocation.ts
@@ -0,0 +1,11 @@
+export interface Housinglocation {
+
+    id: number;
+    name: String;
+    city: string;
+    state: string;
+    photo: string;
+    availableUnits: number;
+    wifi: boolean;
+    laundry: boolean;
+}
